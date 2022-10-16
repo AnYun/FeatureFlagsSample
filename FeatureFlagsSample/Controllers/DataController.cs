28a31125c6f24697e78916fd61ca72e7f831d4b9
AnYun <coding@anyun.dev>
2022-10-16 15:13:03 +0800

    Add Identity Init Controller

---
+++ FeatureFlagsSample/Controllers/DataController.cs
@@ -0,0 +1,65 @@
+﻿using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+
+namespace FeatureFlagsSample.Controllers
+{
+    public class DataController : Controller
+    {
+        private readonly IHttpContextAccessor _httpContextAccessor;
+        private readonly UserManager<IdentityUser> _userManager;
+        private readonly RoleManager<IdentityRole> _roleManager;
+
+        public DataController(IHttpContextAccessor httpContextAccessor, UserManager<IdentityUser> userManager, RoleManager<IdentityRole> roleManager)
+        {
+            _httpContextAccessor = httpContextAccessor;
+            _userManager = userManager;
+            _roleManager = roleManager;
+        }
+
+        public IActionResult Index()
+        {
+            AddRole("User");
+            AddRole("Vip");
+
+            var user = AddUser("user@sample.com");
+            _ = _userManager.AddToRoleAsync(user, "User").Result;
+
+            var vipUser = AddUser("vip@sample.com");
+            _ = _userManager.AddToRoleAsync(vipUser, "User").Result;
+            _ = _userManager.AddToRoleAsync(vipUser, "Vip").Result;
+
+            return Ok();
+        }
+        /// <summary>
+        /// Add Role
+        /// </summary>
+        /// <param name="name"></param>
+        private void AddRole(string name)
+        {
+            var defaultrole = _roleManager.FindByNameAsync(name).Result;
+
+            if (defaultrole == null)
+            {
+                _ = _roleManager.CreateAsync(new IdentityRole(name)).Result;
+            }
+        }
+        /// <summary>
+        /// Add User
+        /// </summary>
+        /// <param name="name"></param>
+        /// <returns></returns>
+        private IdentityUser AddUser(string name)
+        {
+            var user = _userManager.FindByNameAsync(name).Result;
+
+
+            if (user == null)
+            {
+                user = new IdentityUser(name);
+                _=_userManager.CreateAsync(user, "1qaz@WSX").Result;
+            }
+
+            return user;
+        }
+    }
+}
--- FeatureFlagsSample/Program.cs
+++ FeatureFlagsSample/Program.cs
@@ -16,7 +16,8 @@ namespace FeatureFlagsSample
                 options.UseSqlServer(connectionString));
             builder.Services.AddDatabaseDeveloperPageExceptionFilter();
 
-            builder.Services.AddDefaultIdentity<IdentityUser>(options => options.SignIn.RequireConfirmedAccount = true)
+            builder.Services.AddDefaultIdentity<IdentityUser>(options => options.SignIn.RequireConfirmedAccount = false)
+                .AddRoles<IdentityRole>()
                 .AddEntityFrameworkStores<ApplicationDbContext>();
             builder.Services.AddControllersWithViews();
 
